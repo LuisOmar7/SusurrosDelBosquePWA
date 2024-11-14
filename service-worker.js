9423ca7d7f9892196514027b41e440b0afdeabb9
LuisOmar7 <luisomaravalosortiz.77@gmail.com>
2024-11-13 21:44:52 -0600

    Susurros del bosque

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/images/icon-128.png',
+        '/images/icon-512.png'
+      ]).catch((error) => {
+        console.error('Error al agregar recursos al caché:', error);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch((error) => {
+      console.error('Error al manejar la solicitud:', error);
+    })
+  );
+});
